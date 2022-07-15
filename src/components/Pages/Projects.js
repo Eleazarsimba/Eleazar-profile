9c5251f55419910d8a5d97308abf69dfb726ad95
Interns <interns@jambopay.com>
2022-07-16 02:42:37 +0300

    styling

--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
 
 const Projects = () => {
   return (
     <div className='projects'>
       <h3>PROJECTS</h3>
+      <MDBRow>
+      <MDBCol lg='4' md='12' sm='6' xsm='6' className='mb-4'>
+        <img 
+          src='https://mdbootstrap.com/img/new/standard/city/047.webp' 
+          className='img-fluid' 
+          alt='' 
+        />
+      </MDBCol>
+
+      <MDBCol lg='4' md='6' sm='6' xsm='6' className='mb-4'>
+        <img
+          src='https://mdbootstrap.com/img/new/standard/city/047.webp'
+          className='img-fluid'
+          alt=''
+        />
+      </MDBCol>
+    </MDBRow>
     </div>
   )
 }
