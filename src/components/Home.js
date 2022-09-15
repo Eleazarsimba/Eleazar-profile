4301be4031c5446fabe5aa6b1ffe55c347666392
Interns <interns@jambopay.com>
2022-09-15 23:53:07 +0300

    adding project

--- src/components/Home.js
+++ src/components/Home.js
@@ -222,7 +222,7 @@ const Home = () => {
               <h3>PROJECTS</h3>
               <div className="imagerow"> 
               <div className="imagecolumn">
-                  <a href="https://nimble-yeot-0421ef.netlify.app/"> <img src="projects/phone-shop.png" alt='' width='100%' /></a>
+                  <a href="https://nimble-yeot-0421ef.netlify.app/"> <img src="phone-shop.png" alt='' width='100%' /></a>
                   <p>This project is about the selling of mobile phones which proceeds upto to payment with mpesa- 
                     though the paybill number used is only for safaricom daraja, 
                     So don't use it
@@ -240,7 +240,7 @@ const Home = () => {
                 </div>
                 {/* end of second project */}
                 <div className="imagecolumn">
-                  <a href="https://creative-blini-ab1266.netlify.app/"> <img src="projects/lexo-fire.png" alt='' width='100%' /></a>
+                  <a href="https://creative-blini-ab1266.netlify.app/"> <img src="lexo-fire.png" alt='' width='100%' /></a>
                   <p>This project is about management of employees, 
                     adminstration and the sales of petroleum products at Lexo petrol station. 
                     A database admin has to register an admin who can thereafter register other 
@@ -261,7 +261,7 @@ const Home = () => {
                 {/* end of second project */}
               
                 <div className="imagecolumn">
-                  <a href="https://github.com/Eleazarsimba/Swagger-documentation-for-my-APIs"> <img src="projects/swagger-doc.png" alt='' width='100%' /></a>
+                  <a href="https://github.com/Eleazarsimba/Swagger-documentation-for-my-APIs"> <img src="swagger-doc.png" alt='' width='100%' /></a>
                   <p>
                     This is the swagger documentation for all the APIs used in the Lexo petrol station project 
                     <a href="https://github.com/Eleazarsimba/Lexo-react-frontend"> Lexo frontend</a>
